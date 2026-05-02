b908c577e6808d155ad68af26dbdc88aeae98448
waymee <matviigula@gmail.com>
2026-05-03 02:14:07 +0300

    bobriha

--- src/main/java/com/waymee/overdrive/block/cockpit/CockpitBlock.java
+++ src/main/java/com/waymee/overdrive/block/cockpit/CockpitBlock.java
@@ -2,37 +2,28 @@ package com.waymee.overdrive.block.cockpit;
 
 import com.mojang.serialization.MapCodec;
 import com.simibubi.create.content.equipment.wrench.IWrenchable;
+import com.waymee.overdrive.backend.ModShapes;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HorizontalDirectionalBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.phys.BlockHitResult;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.VoxelShape;
-
-import java.util.Map;
 
 public class CockpitBlock extends HorizontalDirectionalBlock implements IWrenchable {
     public static final MapCodec<CockpitBlock> CODEC = simpleCodec(CockpitBlock::new);
 
-    private static final VoxelShape SHAPE_SOUTH = Block.box(1, 0, 5, 15, 5, 16);
-    private static final VoxelShape SHAPE_NORTH = Block.box(1, 0, 0, 15, 5, 11);
-    private static final VoxelShape SHAPE_EAST = Block.box(5, 0, 1, 16, 5, 15);
-    private static final VoxelShape SHAPE_WEST = Block.box(0, 0, 1, 11, 5, 15);
-
-    private static final Map<Direction, VoxelShape> SHAPES = Map.of(
-            Direction.SOUTH, SHAPE_SOUTH,
-            Direction.NORTH, SHAPE_NORTH,
-            Direction.EAST, SHAPE_EAST,
-            Direction.WEST, SHAPE_WEST
-    );
-
     public CockpitBlock(Properties properties) {
         super(properties);
-        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.SOUTH));
+        registerDefaultState(stateDefinition.any().setValue(FACING, Direction.NORTH));
     }
 
     @Override
@@ -42,7 +33,7 @@ public class CockpitBlock extends HorizontalDirectionalBlock implements IWrencha
 
     @Override
     protected VoxelShape getShape(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context) {
-        return SHAPES.get(state.getValue(FACING));
+        return ModShapes.COCKPIT.get(state.getValue(FACING));
     }
 
     @Override
@@ -53,5 +44,13 @@ public class CockpitBlock extends HorizontalDirectionalBlock implements IWrencha
     @Override
     protected MapCodec<? extends HorizontalDirectionalBlock> codec() {
         return CODEC;
+    }
+
+    @Override
+    public InteractionResult useWithoutItem(BlockState state, Level level, BlockPos pos, Player player, BlockHitResult hit) {
+        if (level.isClientSide) {
+            com.waymee.overdrive.client.CockpitInputHandler.setActive(true);
+        }
+        return InteractionResult.SUCCESS;
     }
 }
